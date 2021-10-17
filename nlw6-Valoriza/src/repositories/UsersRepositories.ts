fe9924552ca69f45edd9d372a9f2537b109b83eb
Dg-Lopes <lopesdouglas.dev@gmail.com>
2021-10-17 17:46:14 -0300

    Part of user almost finished

--- nlw6-Valoriza/src/repositories/UsersRepositories.ts
+++ nlw6-Valoriza/src/repositories/UsersRepositories.ts
@@ -2,4 +2,4 @@ import { EntityRepository, Repository } from "typeorm";
 import { User } from "../entities/User";
 
 @EntityRepository(User)
-class UserRepository extends Repository<User> {}
+export class UsersRepositories extends Repository<User> {}
